9def6cd48b3954f9fc88f32312e2eea40e48f2f1
pavlathjeno <pavlathjeno@gmail.com>
2022-12-14 00:12:14 +0100

    add : CPU Cooler properties

--- ComputeYourself/ComputeYourself/Models/CpuCooler.cs
+++ ComputeYourself/ComputeYourself/Models/CpuCooler.cs
@@ -1,6 +1,11 @@
-﻿namespace ComputeYourself.Models
+﻿using System.Net.Sockets;
+using ComputeYourself.Data.Enums.CPU;
+
+namespace ComputeYourself.Models
 {
     public class CpuCooler : BaseModel
     {
+        public int Height { get; set; }
+        public CpuType CpuType { get; set; }
     }
 }
