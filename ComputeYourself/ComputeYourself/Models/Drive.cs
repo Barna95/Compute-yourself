bd296ca4f613cbb0e089d290f221d5f8bb9ac52e
pavlathjeno <pavlathjeno@gmail.com>
2022-12-14 00:49:27 +0100

    add : Drive properties

--- ComputeYourself/ComputeYourself/Models/Drive.cs
+++ ComputeYourself/ComputeYourself/Models/Drive.cs
@@ -1,6 +1,15 @@
-﻿namespace ComputeYourself.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComputeYourself.Models
 {
-    public class Drive
+    public abstract class Drive
     {
+        [Key] public int Id { get; set; }
+        public InstallationTypes InstallationType { get; set; }
+        public DriveSizes DriveSize { get; set; }
+        public ConnectionTypes ConnectionType { get; set; }
+        public int Capacity { get; set; }
+        public int MaxWrite { get; set; }
+        public int MaxRead { get; set; }
     }
 }
