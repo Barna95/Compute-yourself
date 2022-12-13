8b02dad35401acb002492f21dd3f986d8af924d3
pavlathjeno <pavlathjeno@gmail.com>
2022-12-14 00:20:09 +0100

    add : Id and CoolerTypes properties

--- ComputeYourself/ComputeYourself/Models/CpuCooler.cs
+++ ComputeYourself/ComputeYourself/Models/CpuCooler.cs
@@ -1,10 +1,11 @@
-﻿using System.Net.Sockets;
-using ComputeYourself.Data.Enums.CPU;
+﻿using ComputeYourself.Data.Enums.CPU;
 
 namespace ComputeYourself.Models
 {
     public class CpuCooler : BaseModel
     {
+        public int Id { get; set; }
+        public CoolerTypes CoolerType { get; set; }
         public int Height { get; set; }
         public CpuType CpuType { get; set; }
     }
