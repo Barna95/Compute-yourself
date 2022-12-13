089cd9bf76142b80d1ce065957f683c674cab85e
pavlathjeno <pavlathjeno@gmail.com>
2022-12-13 23:57:31 +0100

    add : PSU Id property

--- ComputeYourself/ComputeYourself/Models/PSU.cs
+++ ComputeYourself/ComputeYourself/Models/PSU.cs
@@ -1,9 +1,12 @@
-﻿using ComputeYourself.Data.Enums.PSU;
+﻿using System.ComponentModel.DataAnnotations;
+using ComputeYourself.Data.Enums.PSU;
 
 namespace ComputeYourself.Models
 {
     public class PSU : BaseModel
     {
+        [Key]
+        public int Id { get; set; }
         public int Capacity { get; set; }
         public bool? Modular { get; set; }
         public Classification? Classification { get; set; }
