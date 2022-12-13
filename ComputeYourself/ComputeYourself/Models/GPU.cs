9f540308fb5f45c331c8bc7c3e52aafbfff7d045
pavlathjeno <pavlathjeno@gmail.com>
2022-12-14 00:30:41 +0100

    add : GPU properties

--- ComputeYourself/ComputeYourself/Models/GPU.cs
+++ ComputeYourself/ComputeYourself/Models/GPU.cs
@@ -1,6 +1,15 @@
-﻿namespace ComputeYourself.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComputeYourself.Models
 {
     public class GPU : BaseModel
     {
+        [Key] 
+        public int Id { get; set; }
+        public VideoChipsets VideoChipset { get; set; }
+        public int VramSize { get; set; }
+        public VramTypes VramType { get; set; }
+        public int RequiredPSU { get; set; }
+        public int Length { get; set; }
     }
 }
