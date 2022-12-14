a83bd70e497862dd0bc32a7f161b9f2dc6b4d0a9
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-12-14 13:50:15 +0100

    add : Product tables

--- ComputeYourself/ComputeYourself/Data/AppDbContext.cs
+++ ComputeYourself/ComputeYourself/Data/AppDbContext.cs
@@ -1,8 +1,53 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ComputeYourself.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ComputeYourself.Data
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CPU>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+            modelBuilder.Entity<CpuCooler>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+            modelBuilder.Entity<Drive>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+            modelBuilder.Entity<GPU>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+            modelBuilder.Entity<MotherBoard>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+            modelBuilder.Entity<PcCase>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+            modelBuilder.Entity<PSU>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+            modelBuilder.Entity<RAM>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+
+            base.OnModelCreating(modelBuilder);
+ 
+        }
+
+        public DbSet<CPU> Cpus { get; set; }
+        public DbSet<CpuCooler> CpuCoolers { get; set; }
+        public DbSet<Drive> Drives { get; set; }
+        public DbSet<GPU> Gpus { get; set; }
+        public DbSet<MotherBoard> MotherBoards { get; set; }
+        public DbSet<PcCase> PcCases { get; set; }
+        public DbSet<PSU> Psus { get; set; }
+        public DbSet<RAM> Rams { get; set; }
+
     }
 }
