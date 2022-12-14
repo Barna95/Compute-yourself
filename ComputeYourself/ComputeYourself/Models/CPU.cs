9b3da5b43fe9ddc305557376e40cf81526efaf79
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-12-15 00:09:48 +0100

    update : Unnecessary props

--- ComputeYourself/ComputeYourself/Models/CPU.cs
+++ ComputeYourself/ComputeYourself/Models/CPU.cs
@@ -1,17 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using ComputeYourself.Data.Enums;
 using ComputeYourself.Data.Enums.CPU;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace ComputeYourself.Models
 {
     public class CPU
     {
-        [Key]
         public int Id { get; set; }
 
-        
-        public List<Media> Media { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
@@ -21,18 +20,14 @@ namespace ComputeYourself.Models
         public CpuType Socket { get; set; }
         // public CpuFamilyType CpuFamily { get; set; }
         public bool ManufacturerCooler { get; set; }
-
         // in MB
         public int L3Cache { get; set; }
         public int TotalCache { get; set; }
-
         public int Cores { get; set; }
         public int Threads { get; set; }
-
         // Integrated graphical unit
         public string Igpu { get; set; }
         public int TDP { get; set; }
-
         // in Ghz
         public float CoreClock { get; set; }
         public float TurboCoreClock { get; set; }
