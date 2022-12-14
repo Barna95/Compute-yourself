a9c39040afacc0a0fef332fb1c7f3053dd9ae66b
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-12-14 23:58:32 +0100

    add :  Relationship connections

--- ComputeYourself/ComputeYourself/Data/AppDbContext.cs
+++ ComputeYourself/ComputeYourself/Data/AppDbContext.cs
@@ -36,8 +36,24 @@ namespace ComputeYourself.Data
                 .Property(p => p.Price)
                 .HasColumnType("decimal(18,2)");
 
+            modelBuilder.Entity<PcCase_Media>().HasKey(pm => new
+            {
+                pm.PcCaseId,
+                pm.MediaId
+            });
+
+            modelBuilder.Entity<PcCase_Media>()
+                .HasOne(p => p.PcCase)
+                .WithMany(pm => pm.PcCaseMedias)
+                .HasForeignKey(p => p.PcCaseId);
+
+            modelBuilder.Entity<PcCase_Media>()
+                .HasOne(p => p.Media)
+                .WithMany(pm => pm.PcCaseMedias)
+                .HasForeignKey(p => p.MediaId);
+
             base.OnModelCreating(modelBuilder);
- 
+
         }
 
         public DbSet<CPU> Cpus { get; set; }
@@ -48,6 +64,10 @@ namespace ComputeYourself.Data
         public DbSet<PcCase> PcCases { get; set; }
         public DbSet<PSU> Psus { get; set; }
         public DbSet<RAM> Rams { get; set; }
+
+        // Relationships
+        public DbSet<PcCase_Media> PcCaseMedias { get; set; }
+        public DbSet<Media> Medias { get; set; }
 
     }
 }
