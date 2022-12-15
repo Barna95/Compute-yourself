217b2b33ca5574a38405e142d30d3bab5f6c5905
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-12-15 20:05:14 +0100

    create : GPU service

---
+++ ComputeYourself/ComputeYourself/Data/Services/GPUService.cs
@@ -0,0 +1,14 @@
+﻿using ComputeYourself.Data.Base;
+using ComputeYourself.Models;
+
+namespace ComputeYourself.Data.Services
+{
+    public class GPUService : BaseService<GPU>, IGPUService
+    {
+        private readonly AppDbContext _context;
+
+        public GPUService(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
