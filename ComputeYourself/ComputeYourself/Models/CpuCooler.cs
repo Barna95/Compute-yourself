1587ae602858997bbda6fef0ed5fe45f32a911df
pavlathjeno <pavlathjeno@gmail.com>
2022-12-15 23:14:35 +0100

    add : CpuCooler model
    + Annotations

--- ComputeYourself/ComputeYourself/Models/CpuCooler.cs
+++ ComputeYourself/ComputeYourself/Models/CpuCooler.cs
@@ -9,14 +9,32 @@ namespace ComputeYourself.Models
     public class CpuCooler : IEntityBase
     {
         public int Id { get; set; }
+        [Display(Name = "Cpu cooler name")]
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+        [Display(Name = "Cpu cooler Description")]
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
+        [Display(Name = "Cpu cooler Price")]
+        [Required(ErrorMessage = "Price is required")]
         public decimal Price { get; set; }
+        [Display(Name = "Cpu cooler Brand")]
+        [Required(ErrorMessage = "Brand is required")]
         public ManuFacturerBrand Brand { get; set; }
+        [Display(Name = "Cpu cooler Warranty")]
+        [Required(ErrorMessage = "Warranty is required")]
         public int Warranty { get; set; }
+        [Display(Name = "Cpu cooler Rating")]
+        [Required(ErrorMessage = "Rating is required")]
         public float Rating { get; set; }
+        [Display(Name = "Cpu cooler CoolerType")]
+        [Required(ErrorMessage = "Size is required")]
         public CoolerTypes CoolerType { get; set; }
+        [Display(Name = "Cpu cooler Height")]
+        [Required(ErrorMessage = "Height is required")]
         public int Height { get; set; }
+        [Display(Name = "Cpu cooler CpuType")]
+        [Required(ErrorMessage = "CpuType is required")]
         public CpuType CpuType { get; set; }
 
         //Relationships
