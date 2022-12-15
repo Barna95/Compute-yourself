45cea035b67278ff6d1ea20016d99eb8460940e8
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2022-12-15 11:14:22 +0100

    add : Cpu_Media

---
+++ ComputeYourself/ComputeYourself/Models/Cpu_Media.cs
@@ -0,0 +1,11 @@
+﻿namespace ComputeYourself.Models
+{
+    public class Cpu_Media
+    {
+        public int CpuId { get; set; }
+        public CPU Cpu { get; set; }
+
+        public int MediaId { get; set; }
+        public Media Media { get; set; }
+    }
+}
