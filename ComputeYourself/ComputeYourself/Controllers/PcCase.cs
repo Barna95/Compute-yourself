4e7d3270204280ce704cc46a056042c2c45ae44f
Æmilianus <101416162+Eriknpy@users.noreply.github.com>
2022-12-15 10:39:56 +0100

    create : PcCase controller

---
+++ ComputeYourself/ComputeYourself/Controllers/PcCase.cs
@@ -0,0 +1,21 @@
+﻿using ComputeYourself.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComputeYourself.Controllers
+{
+    public class PcCase : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PcCase(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allPcCases = await _context.PcCases.ToListAsync();
+            return View(allPcCases);
+        }
+    }
+}
