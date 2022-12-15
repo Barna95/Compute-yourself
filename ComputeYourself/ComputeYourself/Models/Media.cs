9fcec3957ed436707c4e425f8632ede447c0c07c
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2022-12-15 11:16:08 +0100

    update : Media with lists of models

--- ComputeYourself/ComputeYourself/Models/Media.cs
+++ ComputeYourself/ComputeYourself/Models/Media.cs
@@ -9,6 +9,13 @@ namespace ComputeYourself.Models
         public string MediaURL { get; set; }
 
         //Relationship
+        public List<Cpu_Media>? Cpu_Medias { get; set; }
+        public List<CpuCooler_Media>? CpuCooler_Medias { get; set; }
+        public List<Drive_Media>? Drive_Medias { get; set; }
+        public List<Gpu_Media>? Gpu_Medias { get; set; }
+        public List<MotherBoard_Media>? MotherBoard_Medias { get; set; }
         public List<PcCase_Media>? PcCaseMedias { get; set; }
+        public List<Psu_Media>? Psu_Medias { get; set; }
+        public List<Ram_Media>? Ram_Medias { get; set; }
     }
 }
