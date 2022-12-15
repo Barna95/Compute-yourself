6bc3ab161998493290fdcce61a3e5dde6a3d0847
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2022-12-15 11:15:00 +0100

    add : Gpu_Media

---
+++ ComputeYourself/ComputeYourself/Models/Gpu_Media.cs
@@ -0,0 +1,11 @@
+﻿namespace ComputeYourself.Models
+{
+    public class Gpu_Media
+    {
+        public int GpuId { get; set; }
+        public GPU Gpu { get; set; }
+
+        public int MediaId { get; set; }
+        public Media Media { get; set; }
+    }
+}
