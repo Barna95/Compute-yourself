9fde877ebe93ee69361c8a8e1e28b343a7aa2146
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-01-12 13:19:01 +0100

    update : navbar link to Cpu-s

--- ComputeYourself/Compute Yourself/frontend/src/layouts/RootLayout.jsx
+++ ComputeYourself/Compute Yourself/frontend/src/layouts/RootLayout.jsx
@@ -4,12 +4,13 @@ import Footer from "../routes/Footer";
 
 export default function RootLayout() {
   return (
-    <div class="root-layout">
+    <div className="root-layout">
       <header>
         <nav>
           <h1>Compute Yourself</h1>
           <NavLink to="/">Home</NavLink>
           <NavLink to="about">About</NavLink>
+          <NavLink to="cpus">Cpus</NavLink>
         </nav>
       </header>
       <main>
