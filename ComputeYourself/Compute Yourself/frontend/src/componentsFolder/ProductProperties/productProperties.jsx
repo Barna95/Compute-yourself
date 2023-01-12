858617094a76f21f03f5a71aae8e35bc80359e62
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-01-12 20:27:42 +0100

    update : ProductProperties
    now uses the ModalWindow component

--- ComputeYourself/Compute Yourself/frontend/src/componentsFolder/ProductProperties/productProperties.jsx
+++ ComputeYourself/Compute Yourself/frontend/src/componentsFolder/ProductProperties/productProperties.jsx
@@ -1,13 +1,13 @@
-﻿const ProductProperties = (props) => {
+﻿import ModalWindow from '../Modal/ModalWindow';
+
+const ProductProperties = (props) => {
     return (
-        <table>
-            <thead>
-                <tr>{props.elementKeys.map(element => <th key={element}>{element}</th>)}</tr>
-            </thead>
-            <tbody>
-                <tr>{props.elementKeys.map(element => <td key={element}>{props.dataProperties[element]}</td>)}</tr>
-            </tbody>
-        </table>);
-}
+        <div>
+            <ModalWindow dataProperties={props.dataProperties}
+                elementKeys={props.elementKeys}
+                />
+        </div>
+        )
+};
 
 export default ProductProperties;
