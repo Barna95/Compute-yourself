a0c1b52c2848b35090f618cd7ff0bc0c52d183db
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-01-12 13:17:23 +0100

    create : ProductProperties

---
+++ ComputeYourself/Compute Yourself/frontend/src/componentsFolder/ProductProperties/productProperties.jsx
@@ -0,0 +1,13 @@
+﻿const ProductProperties = (props) => {
+    return (
+        <table>
+            <thead>
+                <tr>{props.elementKeys.map(element => <th key={element}>{element}</th>)}</tr>
+            </thead>
+            <tbody>
+                <tr>{props.elementKeys.map(element => <td key={element}>{props.dataProperties[element]}</td>)}</tr>
+            </tbody>
+        </table>);
+}
+
+export default ProductProperties;
