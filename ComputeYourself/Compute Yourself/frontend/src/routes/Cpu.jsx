f86b3ec3f8f0415345aba4562d0d6d928b45a5b1
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-01-12 13:17:52 +0100

    create : Cpu route

---
+++ ComputeYourself/Compute Yourself/frontend/src/routes/Cpu.jsx
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ProductProperties from "../componentsFolder/ProductProperties/productProperties" 
+
+export default function Cpu() {
+    const [data, setData] = useState("");
+    useEffect(() => {
+        axios.get("https://localhost:7195/product").then((response) => {
+            response.data.map((e) => {
+                setData(e);
+            });
+        });
+
+    }, []);
+    const elements = Object.keys(data);
+    return (
+        <div className='cpus'>
+            <ProductProperties dataProperties={data} elementKeys={elements} />
+        </div>
+    )
+}
