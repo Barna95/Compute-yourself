62e0e325f5b28751061c755c71f1c642d879bf29
Erik <101416162+Eriknpy@users.noreply.github.com>
2023-01-12 12:08:38 +0100

    create : Routes

---
+++ ComputeYourself/Compute Yourself/frontend/src/routes/About.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function about() {
+  return (
+    <div className='about'>This is the <span className="select">about</span> page</div>
+  )
+}
---
+++ ComputeYourself/Compute Yourself/frontend/src/routes/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function home() {
+  return (
+    <div className='home'>
+        This is the <span className="select">home</span> page
+    </div>
+  )
+}
