4cd0a79eea62d8ceb7853a1e4a6634d586f0b63d
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-01-23 18:24:58 +0100

    update : ModalWindow
    different types of tries how to work with the Json Object.

--- ComputeYourself/Compute Yourself/frontend/src/componentsFolder/Modal/ModalWindow.jsx
+++ ComputeYourself/Compute Yourself/frontend/src/componentsFolder/Modal/ModalWindow.jsx
@@ -1,74 +1,63 @@
-﻿import * as React from "react";
+﻿import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Modal from "@mui/material/Modal";
-import Table from 'react-bootstrap/Table';
-
-// --primary: #bc4123;
-//   --secondary: #2b3452;
 const style = {
-  display: "flex",
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  bgcolor: "#2b3452",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-  color: "#bc4123",
+    display: 'flex',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    color : 'lightblue',
+    bgcolor: 'lightred',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
 };
 
 export default function ModalWindow(props) {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+    const [data, setData] = useState([]);
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    let productValues = []
+    const selectValues = Object.values(props.dataProperties).map((e) => productValues.push(Object.values(e)));
+    let productKeys = [];
+    const selectKeys = Object.keys(props.dataProperties.map((e) => productKeys.push(Object.keys(e))));
 
-  const mystyle = {
-    color: "#bc4123",
-    backgroundColor: "#2b3452",
-    padding: "10px",
-    fontFamily: "Arial",
-  };
+    useEffect(() => {
+        axios.get("https://localhost:7195/product/cpu").then(
+            (response) => {
+                setData(response.data);
+            });
+    }, []);
 
-  const tableCell = {
-    color: "white",
-  };
-
-  const modalButton = {
-    color: 'white',
-    fontFamily: "Arial",
-  };
-  
-  return (
-    <div>
-      <Button style={modalButton} onClick={handleOpen}>{props.dataProperties.name}</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <table>
-            <thead>
-              <tr>
-                {props.elementKeys.map((element) => (
-                  <th style={tableCell} key={element}>{element}</th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                {props.elementKeys.map((element) => (
-                  <td style={tableCell} key={element}>{props.dataProperties[element]}</td>
-                ))}
-              </tr>
-            </tbody>
-          </table>
-        </Box>
-      </Modal>
-    </div>
-  );
+    // id alapú lekérdezés show keys -> {Object.keys(data).map((asd, idx) => <p key={idx}> {asd }</p> ) }
+    // id alapú lekérdezés show actual data -> {Object.keys(data).map((asd, idx) => <p key={idx}> {data[asd] }</p> ) } 
+    // ami most bent van az az összes adatból való magic, not so good.
+    return (
+        <div>
+            {props.dataProperties.map((value, index) => <Button onClick={handleOpen} key={index}> {value.name} </Button>)}
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>                  
+                    <table>
+                        <thead>
+                            <tr>{productKeys.map((key, idx) => <td key={idx}>{key}</td> )}</tr>
+                        </thead>
+                        <tbody>
+                            <tr>{productValues.map((value, idx) => <td key={idx}>{value}</td>)}</tr>
+                        </tbody>
+                    </table>
+                </Box>
+            </Modal>
+        </div>
+    );
 }
