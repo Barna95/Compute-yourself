df534cbdd53371e2d80ff47ec5c649106c07e208
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-01-23 18:29:51 +0100

    update : main
    added MUI container

--- ComputeYourself/Compute Yourself/frontend/src/main.jsx
+++ ComputeYourself/Compute Yourself/frontend/src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Container } from "@mui/material"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+    <React.StrictMode>
+        <Container maxWidth={false} sx={{ bgcolor: "lightgrey" }}>
+            <App />
+        </Container>
   </React.StrictMode>,
 )
+
+   
