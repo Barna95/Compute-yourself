c7b049b721f5fb2212450e48e7181a8940d628a6
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-01-24 15:09:24 +0100

    create : ShowProductDetails
    generally fetches the needed product, and shows its data

---
+++ ComputeYourself/Compute Yourself/frontend/src/routes/ShowProductDetails.jsx
@@ -0,0 +1,41 @@
+﻿import * as React from 'react';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useLocation, useNavigate } from "react-router-dom";
+
+
+
+export default function ShowProductDetails() {
+    const [data, setData] = useState([]);
+    const location = useLocation();
+    const navigate = useNavigate();
+    // From all objects -> 
+    //let productValues = []
+    //const selectValues = Object.values(props.dataProperties).map((e) => productValues.push(Object.values(e)));
+    //let productKeys = [];
+    //const selectKeys = Object.keys(props.dataProperties.map((e) => productKeys.push(Object.keys(e))));
+    useEffect(() => {
+        axios.get(`https://localhost:7195/product/${location.state.productType}/${location.state.itemId}`).then(
+            (response) => {
+                setData(response.data);
+            });
+    }, []);
+
+    //From 1 object ->
+    // show keys -> {Object.keys(data).map((asd, idx) => <p key={idx}> {asd }</p> ) }
+    // show values -> {Object.keys(data).map((asd, idx) => <p key={idx}> {data[asd] }</p> ) } 
+    // ami most bent van az az összes adatból való magic, not so good.
+    return (
+        <div>
+            <button onClick={() => navigate("/product") }> go back </button>
+            <table>
+                <thead>
+                    <tr>{Object.keys(data).map((propName, idx) => <td key={idx}> {propName}</td>)}</tr>
+                </thead>
+                <tbody>
+                    <tr>{Object.keys(data).map((propName, idx) => <td key={idx}> {data[propName]}</td>)}</tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
