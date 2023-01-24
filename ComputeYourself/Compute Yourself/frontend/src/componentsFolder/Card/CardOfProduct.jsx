281b2bb1583b3d98b814f9c0d0c4e03bc719a74a
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-01-24 15:05:54 +0100

    create : CardOfProduct
    what to show on a card of a product

---
+++ ComputeYourself/Compute Yourself/frontend/src/componentsFolder/Card/CardOfProduct.jsx
@@ -0,0 +1,37 @@
+﻿import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const Card = (props) => {
+    const [productId, setProductId] = useState([]);
+    const navigate = useNavigate();
+    const neededValues = [];
+    const fillNeededValues =
+        props.dataProperties.map((value, index) => {
+            neededValues.push(<div key={index}>{value.name} </div>,
+                <div>{value.price}</div>,
+                <div>{value.rating}</div>)
+        });
+
+    useEffect(() => {
+        fillNeededValues;
+    }, []);
+
+    useEffect(() => {
+        Object.keys(props.dataProperties.map((e, idx) => setProductId(idx + 1)));
+    }, [neededValues])
+
+
+    return (
+        <div>
+            <div>{neededValues}</div>
+            <button
+                title="Go to Details"
+                onClick={ () => navigate(`/product/details`, { replace: true, state: { itemId: productId, productType: props.productType }}
+                )}
+            >Details</button>
+        </div>
+    )
+};
+
+export default Card;
