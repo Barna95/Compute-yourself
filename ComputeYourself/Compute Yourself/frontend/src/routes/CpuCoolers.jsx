1fbe3c4b1b18b7c6473e830034af24b74297733c
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-01-26 16:52:43 +0100

    update : CpuCoolers

--- ComputeYourself/Compute Yourself/frontend/src/routes/CpuCoolers.jsx
+++ ComputeYourself/Compute Yourself/frontend/src/routes/CpuCoolers.jsx
@@ -2,9 +2,11 @@
 import Card from "../componentsFolder/Card/CardOfProduct"
 import React from 'react';
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function CpuCoolers() {
     const [data, setData] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get("https://localhost:7195/product/cpucooler").then(
@@ -15,6 +17,11 @@ export default function CpuCoolers() {
     
     return (
         <div className='cpucoolers'>
+            <button
+                title="Go to Details"
+                onClick={() => navigate(`/product/addnewcpucooler`, { replace: true, state: { formData: data, productType: "cpucooler" } }
+                )}
+            >Add new</button>
             <Card dataProperties={data} productType="cpucooler" />
         </div>
     )
