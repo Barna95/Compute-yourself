a42cf8ff563491fc5d1f4917d254006cf07e0f05
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-01-26 16:53:34 +0100

    update : Psus

--- ComputeYourself/Compute Yourself/frontend/src/routes/Psus.jsx
+++ ComputeYourself/Compute Yourself/frontend/src/routes/Psus.jsx
@@ -1,11 +1,12 @@
 ﻿import { useEffect, useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 import Card from "../componentsFolder/Card/CardOfProduct"
 import React from 'react';
 import axios from "axios";
 
 export default function Psus() {
     const [data, setData] = useState([]);
+    const navigate = useNavigate();
     useEffect(() => {
         axios.get("https://localhost:7195/product/psu").then(
             (response) => {
@@ -15,6 +16,11 @@ export default function Psus() {
    
     return (
         <div className='psus'>
+            <button
+                title="Go to Details"
+                onClick={() => navigate(`/product/addnewpsu`, { replace: true, state: { formData: data, productType: "psu" } }
+                )}
+            >Add new</button>
             <Card dataProperties={data} productType="psu" />
         </div>
     )
