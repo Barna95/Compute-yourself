875d1ba75075462807cc2f2ddcd6dbff643c57db
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-01-26 16:53:24 +0100

    update : PcCases

--- ComputeYourself/Compute Yourself/frontend/src/routes/PcCases.jsx
+++ ComputeYourself/Compute Yourself/frontend/src/routes/PcCases.jsx
@@ -1,11 +1,12 @@
 ﻿import { useEffect, useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 import Card from "../componentsFolder/Card/CardOfProduct"
 import React from 'react';
 import axios from "axios";
 
 export default function PcCases() {
     const [data, setData] = useState([]);
+    const navigate = useNavigate();
     useEffect(() => {
         axios.get("https://localhost:7195/product/pccase").then(
             (response) => {
@@ -15,6 +16,11 @@ export default function PcCases() {
    
     return (
         <div className='cpus'>
+            <button
+                title="Go to Details"
+                onClick={() => navigate(`/product/addnewpccase`, { replace: true, state: { formData: data, productType: "pccase" } }
+                )}
+            >Add new</button>
             <Card dataProperties={data} productType="pccase" />
         </div>
     )
