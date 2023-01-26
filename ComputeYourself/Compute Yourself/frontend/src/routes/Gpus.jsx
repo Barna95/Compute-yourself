58c9766752cacd65ef3d329030cd764a805061e2
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-01-26 16:52:57 +0100

    update : Gpus

--- ComputeYourself/Compute Yourself/frontend/src/routes/Gpus.jsx
+++ ComputeYourself/Compute Yourself/frontend/src/routes/Gpus.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 import Card from "../componentsFolder/Card/CardOfProduct"
 import React from 'react';
 import axios from "axios";
 
 export default function Gpus() {
     const [data, setData] = useState([]);
+    const navigate = useNavigate();
     useEffect(() => {
         axios.get("https://localhost:7195/product/gpu").then(
             (response) => {
@@ -15,6 +16,11 @@ export default function Gpus() {
 
     return (
         <div className='cpus'>
+            <button
+                title="Go to Details"
+                onClick={() => navigate(`/product/addnewgpu`, { replace: true, state: { formData: data, productType: "gpu" } }
+                )}
+            >Add new</button>
             <Card dataProperties={data} productType="gpu" />
         </div>
     )
