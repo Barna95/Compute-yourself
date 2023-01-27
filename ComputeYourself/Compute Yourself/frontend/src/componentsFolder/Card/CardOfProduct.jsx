35e27aae179f598335097302d3e6a998618e9d03
Erik <101416162+Eriknpy@users.noreply.github.com>
2023-01-27 05:25:29 +0100

    style : Re-design product card

--- ComputeYourself/Compute Yourself/frontend/src/componentsFolder/Card/CardOfProduct.jsx
+++ ComputeYourself/Compute Yourself/frontend/src/componentsFolder/Card/CardOfProduct.jsx
@@ -4,11 +4,15 @@ import Box from '@mui/material/Box';
 import Button from '@material-ui/core/Button';
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from '@mui/material/CardMedia';
+import DeleteButtonCard from "../../routes/DeleteProducts/DeleteButtonCard";
+import EditIcon from '@mui/icons-material/Edit';
 import Grid from "@material-ui/core/Grid";
 import Paper from "@mui/material/Paper";
 import PropTypes from "prop-types";
 import Rating from '@mui/material/Rating';
+import Stack from '@mui/material/Stack';
 import Typography from "@material-ui/core/Typography";
+import VisibilityIcon from '@mui/icons-material/Visibility';
 import { useNavigate } from "react-router-dom";
 import { withStyles } from '@material-ui/core/styles';
 
@@ -43,33 +47,40 @@ const Card = (props) => {
         {props.dataProperties.map((value, index) =>
             <Grid item md={6}>
                 <Paper key={index} elevation={18}>
-                
-                    <CardMedia
-                        sx={{ height: 330 }}
-                        image={value.mainImage?value.mainImage:"https://via.placeholder.com/400x150.png"}
-                    />
-                <CardContent>
-                
-                        <Typography noWrap="true">{value.name}</Typography>
-                        <Typography noWrap="true">{Math.round((value.price)*100)/100} €</Typography>
-                        <Typography noWrap="true">Rating</Typography>
+                        <CardMedia
+                          sx={{ height: 330 }}
+                          image={value.mainImage?value.mainImage:"https://via.placeholder.com/400x150.png"}/>
+                        <CardContent>
+                        <Typography noWrap="true" align='center' variant='subtitle1'>{value.name}</Typography>
+                        <Typography noWrap="true" align='center' variant='h6'>{Math.round((value.price)*100)/100} €</Typography>
+                        {/* <Typography noWrap="true" variant='justify'>Rating </Typography> */}
                         
-                        <Rating name="read-only" value={value.rating} readOnly />
+                        <Stack direction="column" spacing={1}>
+                        <Typography align='left' variant='body1'>Rating</Typography>
+                        <Rating precision={0.5} name="read-only" value={value.rating?value.rating:null} readOnly />
+                        
+                        </Stack>
+                        <Stack direction="row-reverse"   align='center' spacing={1}>
                         <Button variant="outlined" size="small"
                             title="Go to Details"
                             onClick={() => {
                               localStorage.setItem("productType", props.productType)
                               return navigate(`/product/details`, { replace: true, state: { itemId: value.id, productType: props.productType } }
                             )}}
-                        >Details
+                        ><VisibilityIcon fontSize="small"></VisibilityIcon> Details
                         </Button>
                         <Button 
                           variant="outlined" 
                           size="small" 
                           title="Go to Edit"
-                          onClick={() => {localStorage.setItem("itemId", value.id); return navigate(`/product/${props.productType}/edit`, { replace: true, state: { itemId: value.id } })}}>
-                          Edit
+                          color="primary"
+                          onClick={() => {localStorage.setItem("itemId", value.id); 
+                          return navigate(`/product/${props.productType}/edit`, { replace: true, state: { itemId: value.id } })}}>
+                          <EditIcon color="primary"  fontSize="small"></EditIcon> Edit
                         </Button>
+                        <DeleteButtonCard productType={props.productType} productId={value.id}/>
+                        
+                        </Stack>
                     </CardContent>
                 </Paper>
             </Grid>
