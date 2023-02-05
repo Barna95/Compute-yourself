e1dfe5b4d930e3f2f84092cb4a77aaf08038b7de
Erik <101416162+Eriknpy@users.noreply.github.com>
2023-02-05 22:31:28 +0100

    create : TokenService interface

---
+++ ComputeYourself/Compute Yourself/Data/Services/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using ComputeYourself.Models.Identity;
+
+namespace ComputeYourself.Data.Services
+{
+    public interface ITokenService
+    { 
+        Task<string>  GenerateToken(User user);
+    }
+}
