bcf0a753715cb631943de12f7daf093ea6a7273a
Erik <101416162+Eriknpy@users.noreply.github.com>
2023-02-05 22:24:38 +0100

    update : UserRoles class to static

--- ComputeYourself/Compute Yourself/Static/UserRoles.cs
+++ ComputeYourself/Compute Yourself/Static/UserRoles.cs
@@ -1,6 +1,6 @@
 ﻿namespace ComputeYourself.Static
 {
-    public class UserRoles
+    public static class UserRoles
     {
         public const string Admin = "Admin";
         public const string Member = "Member";
