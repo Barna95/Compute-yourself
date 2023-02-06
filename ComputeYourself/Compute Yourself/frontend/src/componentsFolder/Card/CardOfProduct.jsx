2d1a8b033a1d3ed2826a83294605df432f159e2a
pavlathjeno <pavlathjeno@gmail.com>
2023-02-06 16:00:52 +0100

    Update : Product links and pictures

--- ComputeYourself/Compute Yourself/frontend/src/componentsFolder/Card/CardOfProduct.jsx
+++ ComputeYourself/Compute Yourself/frontend/src/componentsFolder/Card/CardOfProduct.jsx
@@ -11,7 +11,7 @@ import Rating from '@mui/material/Rating';
 import Stack from '@mui/material/Stack';
 import Typography from "@material-ui/core/Typography";
 import VisibilityIcon from '@mui/icons-material/Visibility';
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = (theme) => ({
@@ -44,38 +44,46 @@ const Card = (props) => {
         {props.dataProperties.map((value, index) =>
             <Grid item md={6} key={index }>
                 <Paper key={index} elevation={18}>
-                        <CardMedia
-                          sx={{ height: 330 }}
-                          image={value.mainImage?value.mainImage:"https://via.placeholder.com/400x150.png"}/>
-                        <CardContent>
-                        <Typography noWrap="true" align='center' variant='subtitle1'>{value.name}</Typography>
-                        <Typography noWrap="true" align='center' variant='h6'>{Number(value.price).toFixed(2)} €</Typography>
-                        {/* <Typography noWrap="true" variant='justify'>Rating </Typography> */}
-                        
-                        <Stack direction="column" spacing={1}>
-                        <Typography align='left' variant='body1'>Rating</Typography>
-                        <Rating precision={0.5} name="read-only" value={value.rating?value.rating:null} readOnly />
-                        
-                        </Stack>
-                        <Stack direction="row-reverse"   align='center' spacing={1}>
-                        <Button variant="outlined" size="small"
-                            title="Go to Details"
-                            onClick={() => {return navigate(`/product/${props.productType}/${value.id}/details`)}}
-                        ><VisibilityIcon fontSize="small"></VisibilityIcon> Details
-                        </Button>
-                        <Button 
-                          variant="outlined" 
-                          size="small" 
-                          title="Go to Edit"
-                          color="primary"
-                          onClick={() => {return navigate(`/product/${props.productType}/${value.id}/edit`)}}>
-                          <EditIcon color="primary"  fontSize="small"></EditIcon> Edit
-                        </Button>
-                        <DeleteButtonCard productType={props.productType} productId={value.id}/>
-                        
-                        </Stack>
-                    </CardContent>
-                </Paper>
+                  <CardContent>
+                      <Box height={280} sx={{  display: 'flex', justifyContent: 'center' }}>
+                        <CardMedia 
+                          component="img"
+                          sx={{ height: 240, width: 'auto' }}
+                          image={value.mainImage?value.mainImage:"https://via.placeholder.com/400x150.png"}
+                          onClick={() => {return navigate(`/product/${props.productType}/${value.id}/details`)}}
+                          />
+                      </Box>
+                    <Typography noWrap="true" align='center' variant='subtitle1'>
+                      <Link to={`/product/${props.productType}/${value.id}/details`}>{value.name }</Link>
+                    </Typography>
+                    {/* <Typography onClick={() => {return navigate(`/product/${props.productType}/${value.id}/details`)}} noWrap="true" align='center' variant='subtitle1'>{value.name}</Typography> */}
+                    
+                    <Typography noWrap="true" align='center' variant='h6'>{Number(value.price).toFixed(2)} €</Typography>
+                    {/* <Typography noWrap="true" variant='justify'>Rating </Typography> */}
+                    
+                    <Stack direction="column" spacing={1}>
+                      <Typography align='left' variant='body1'>Rating</Typography>
+                      <Rating precision={0.5} name="read-only" value={value.rating?value.rating:null} readOnly />
+                    
+                    </Stack>
+                    <Stack direction="row-reverse"   align='center' spacing={1}>
+                      <Button variant="outlined" size="small"
+                          title="Go to Details"
+                          onClick={() => {return navigate(`/product/${props.productType}/${value.id}/details`)}}>
+                        <VisibilityIcon fontSize="small"></VisibilityIcon> Details
+                      </Button>
+                      <Button 
+                        variant="outlined" 
+                        size="small" 
+                        title="Go to Edit"
+                        color="primary"
+                        onClick={() => {return navigate(`/product/${props.productType}/${value.id}/edit`)}}>
+                        <EditIcon color="primary"  fontSize="small"></EditIcon> Edit
+                      </Button>
+                      <DeleteButtonCard productType={props.productType} productId={value.id}/>
+                    </Stack>
+                </CardContent>
+              </Paper>
             </Grid>
         )
       }
