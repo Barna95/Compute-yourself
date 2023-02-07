0577cc1a6dd3e01c39ad743aec3b7da3a103dd6b
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-02-07 17:57:45 +0100

    update : Rams
    role based authentication

--- ComputeYourself/Compute Yourself/frontend/src/routes/Rams.jsx
+++ ComputeYourself/Compute Yourself/frontend/src/routes/Rams.jsx
@@ -7,6 +7,7 @@ import Grid from "@mui/material/Grid";
 import React from 'react';
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth"
 
 export default function Rams() {
     const [data, setData] = useState([]);
@@ -18,15 +19,26 @@ export default function Rams() {
             });
     }, [data]);
 
+    const { auth } = useAuth();
+    const isAdmin = auth?.roles?.includes("Admin");
+    const loggedIn = localStorage.getItem("isLoggedIn");
+    const adminField = [];
+    if (isAdmin && loggedIn === "true") {
+        adminField.push(
+            <Grid item md={10}>
+                <Button variant="outlined" size="medium"
+                    title="Go to Details"
+                    onClick={() => navigate(`/product/addnewram`, { replace: true, state: { formData: data, productType: "ram" } }
+                    )}
+                ><AddIcon color="success" ></AddIcon>Ram</Button>
+            </Grid>
+        );
+    }
+
+
     return (
         <>
-            <Grid item md={10}>
-            <Button variant="outlined" size="medium" 
-                title="Go to Details"
-                onClick={() => navigate(`/product/addnewram`, { replace: true, state: { formData: data, productType: "ram" } }
-                )}
-            ><AddIcon color="success" ></AddIcon>Ram</Button>
-            </Grid>
+            {adminField }
             <Card dataProperties={data} productType="ram" />
         </>
     )
