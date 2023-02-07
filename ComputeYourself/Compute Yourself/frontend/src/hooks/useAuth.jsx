787c6ebf2fedea62a298e2a81e697b2382147e92
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-02-07 13:13:06 +0100

    create : useAuth

---
+++ ComputeYourself/Compute Yourself/frontend/src/hooks/useAuth.jsx
@@ -0,0 +1,8 @@
+﻿import { useContext } from "react";
+import AuthContext from "../Authentication/AuthContext/AuthProvider";
+
+const useAuth = () => {
+    return useContext(AuthContext);
+}
+
+export default useAuth;
