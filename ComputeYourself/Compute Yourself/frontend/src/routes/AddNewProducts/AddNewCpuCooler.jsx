c5c00637f3dac63c15e7d5b471e9734e20478c80
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-02-07 17:53:57 +0100

    update : AddNewCpuCooler
    using JWT token at fetch

--- ComputeYourself/Compute Yourself/frontend/src/routes/AddNewProducts/AddNewCpuCooler.jsx
+++ ComputeYourself/Compute Yourself/frontend/src/routes/AddNewProducts/AddNewCpuCooler.jsx
@@ -2,6 +2,7 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useState } from 'react';
+import useAuth from "../../hooks/useAuth"
 
 export default function AddNewCpuCooler() {
     const [formValues, setFormValues] = useState({
@@ -22,16 +23,21 @@ export default function AddNewCpuCooler() {
     const keys = Object.keys(formValues).map((propName, idx) => { return propName });
     let handleChange = (e) => {
         setFormValues({ ...formValues, [e.target.name]: e.target.value });
-        console.log(formValues);
     }
 
-    let handleSubmit = async () => {
+    const { auth } = useAuth();
+
+    let handleSubmit = async (e) => {
+        e.preventDefault
+        const token = auth.token
         const json = JSON.stringify(formValues);
         await axios.post(`https://localhost:7195/product/cpucooler`, json, {
             headers: {
                 'Content-Type': 'application/json;charset=UTF-8',
+                'Authorization': `Bearer ${token}`
             }
         })
+        return navigate("/product/cpucooler")
     };
     // TODO -> input fields be required as its needed.
     // create the input nodes with map but it would be hard to change the different
@@ -39,7 +45,7 @@ export default function AddNewCpuCooler() {
     // so ill just leave it here for now.
     // keys.map((propName, idx) => <div key={idx}> {propName}<input placeholder="" aria-label="{propName}" type="text" name={propName} onChange={e => handleChange(e)} /></div>)
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={e => handleSubmit(e)}>
             <div> Name <input placeholder="" aria-label="{keys[3]}" type="text" name={keys[3]} onChange={e => handleChange(e)} required /></div>
             <div> Description <input placeholder="" aria-label="{keys[4]}" type="text" name={keys[4]} onChange={e => handleChange(e)} required /></div>
             <div> Price <input placeholder="" aria-label="{keys[5]}" type="number" name={keys[5]} onChange={e => handleChange(e)} required /></div>
