ee93a991e891d1c73c32f18d92fe297b2a902f01
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-02-07 13:13:22 +0100

    update : Main
    with AuthProvider

--- ComputeYourself/Compute Yourself/frontend/src/main.jsx
+++ ComputeYourself/Compute Yourself/frontend/src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 //import './index.css'
+import { AuthProvider } from './Authentication/AuthContext/AuthProvider';
 import { Container } from "@mui/material"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+        <AuthProvider>
             <App />
+        </AuthProvider>
   </React.StrictMode>,
 )
 
