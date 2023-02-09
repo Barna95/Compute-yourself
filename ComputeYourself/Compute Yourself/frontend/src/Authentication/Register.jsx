fb934c5594f0e37aa831064c5e0accbed241052c
pavlathjeno <pavlathjeno@gmail.com>
2023-02-09 23:33:57 +0100

    Update : Rework sign up page with mui

--- ComputeYourself/Compute Yourself/frontend/src/Authentication/Register.jsx
+++ ComputeYourself/Compute Yourself/frontend/src/Authentication/Register.jsx
@@ -3,9 +3,28 @@ import Button from '@material-ui/core/Button';
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { toast } from 'react-toastify';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import PasswordStrengthBar from 'react-password-strength-bar';
+import { withStyles } from '@material-ui/core/styles';
 
 export default function Register() {
     //account/login account/register
+    const CustomPasswordStrengthBar = withStyles({
+        root: {
+          height: '15px',
+          width: '200px',
+        },
+      })(PasswordStrengthBar);
     const navigate = useNavigate();
     const [data, setData] = useState({
         "userName": "",
@@ -63,13 +82,75 @@ export default function Register() {
     return (
         <>
             <form onSubmit={e => handleSubmit(e) }>
-                <div> Name <input placeholder="" aria-label="userName" type="text" name="userName" onChange={e => handleChange(e)} required /></div>
-                <div> Password <input placeholder="" aria-label="password" type="text" name="password" onChange={e => handleChange(e)} required /></div>
-                <div> E-mail <input placeholder="" aria-label="email" type="text" name="email" onChange={e => handleChange(e)} required /></div>
-                    <div className="button-section">
-                        <Button type="submit" variant="outlined" size="small">Register</Button>
-                        <Button type="button" variant="outlined" size="small" onClick={() => navigate("/")}> Back </Button>
-                    </div>
+                <Container component="main" maxWidth="xs">
+                    <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        }}
+                        >
+                    <Avatar sx={{ m: 1, bgcolor: 'grey' }}>
+                    <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                    <Box sx={{ mt: 3 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                autoComplete="given-name"
+                                required
+                                fullWidth
+                                id="userName"
+                                label="UserName"
+                                autoFocus
+                                name="userName" onChange={e => handleChange(e)}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                autoComplete="email"
+                                name="email" onChange={e => handleChange(e)}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                required
+                                fullWidth
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="new-password"
+                                name="password" onChange={e => handleChange(e)}
+                                />
+                                <CustomPasswordStrengthBar password={data.password} />
+                            </Grid>
+                        </Grid>
+                        <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                        >
+                        Sign Up
+                        </Button>
+                        <Grid container justifyContent="flex-end">
+                            <Grid item>
+                                <Link href="#" variant="body2" onClick={() => navigate(`/account/login`)}>
+                                Already have an account? Sign in
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                    </Box>
+                </Container>
             </form>
         </>
         )
