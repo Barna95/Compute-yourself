469c9748d80330d16b99cd98ab7dce823f2468d0
pavlathjeno <pavlathjeno@gmail.com>
2023-02-09 08:56:31 +0100

    Update : Rework edit cpu cooler forms

--- ComputeYourself/Compute Yourself/frontend/src/routes/EditProducts/EditCpuCooler.jsx
+++ ComputeYourself/Compute Yourself/frontend/src/routes/EditProducts/EditCpuCooler.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 import useAuth from "../../hooks/useAuth"
+import themeStyle from "../../themeStyle"
+import Button from '@material-ui/core/Button';
+import TextField from '@mui/material/TextField';
+import { Grid } from "@mui/material";
 
 export default function EditCpuCooler() {
     const [data, setData] = useState([]);
@@ -37,6 +41,27 @@ export default function EditCpuCooler() {
 
     return (
         <form onSubmit={e => handleSubmit(e)}>
+            <Grid container md={12} alignContent="center">
+                <Grid item md={6} padding={2} autoComplete="off" noValidate>
+                <TextField style={themeStyle.textInput} required id="outlined-required" variant="outlined" size = "small" label="Name" placeholder={data[keys[4]]} defaultValue=" " value={data[keys[4]]} name={keys[4]} onChange={e => handleChange(e)}/>
+                <TextField style={themeStyle.textInput} required id="outlined-required" variant="outlined" size = "small" label="Description" placeholder={data[keys[5]]} defaultValue=" " value={data[keys[5]]} name={keys[5]} onChange={e => handleChange(e)}/>
+                <TextField style={themeStyle.textInput} required id="outlined-number" type="number" variant="outlined" size = "small" label="Price" placeholder={data[keys[6]]} defaultValue="0" value={data[keys[6]]} name={keys[6]} onChange={e => handleChange(e)}/>
+                <TextField style={themeStyle.textInput} required id="outlined-required" variant="outlined" size = "small" label="Brand" placeholder={data[keys[7]]} defaultValue=" " value={data[keys[7]]} name={keys[7]} onChange={e => handleChange(e)}/>
+                <TextField style={themeStyle.textInput} required id="outlined-number" type="number" variant="outlined" size = "small" label="Warranty" placeholder={data[keys[8]]} defaultValue="0" value={data[keys[8]]} name={keys[8]} onChange={e => handleChange(e)}/>
+                <TextField style={themeStyle.textInput} required id="outlined-number" type="number" variant="outlined" size = "small" label="Rating" placeholder={data[keys[9]]} defaultValue="0" value={data[keys[9]]} name={keys[9]} onChange={e => handleChange(e)}/>
+                <TextField style={themeStyle.textInput} required id="outlined-required" variant="outlined" size = "small" label="Cooler Type" placeholder={data[keys[0]]} defaultValue=" " value={data[keys[0]]} name={keys[0]} onChange={e => handleChange(e)}/>
+                <TextField style={themeStyle.textInput} required id="outlined-required" variant="outlined" size = "small" label="Height" placeholder={data[keys[1]]} defaultValue=" " value={data[keys[1]]} name={keys[1]} onChange={e => handleChange(e)}/>
+                <TextField style={themeStyle.textInput} required id="outlined-required" variant="outlined" size = "small" label="Cpu Type" placeholder={data[keys[2]]} defaultValue=" " value={data[keys[2]]} name={keys[2]} onChange={e => handleChange(e)}/>
+                <TextField style={themeStyle.textInput} required id="outlined-required" variant="outlined" size = "small" label="Main Image" placeholder={data[keys[10]]} defaultValue=" " value={data[keys[10]]} name={keys[10]} onChange={e => handleChange(e)}/>
+                <TextField style={themeStyle.textInput} required id="outlined-required" variant="outlined" size = "small" label="Product Official Website" placeholder={data[keys[11]]} defaultValue=" " value={data[keys[11]]} name={keys[11]} onChange={e => handleChange(e)}/>
+                <TextField style={themeStyle.textInput} required id="outlined-required" variant="outlined" size = "small" label="Model Number" placeholder={data[keys[12]]} defaultValue=" " value={data[keys[12]]} name={keys[12]} onChange={e => handleChange(e)}/>
+                <Grid item md={2}>
+                    <Button style={themeStyle.navbarButton} variant="outlined" size="small" type="submit">Save</Button>
+                    <Button style={themeStyle.navbarButton} variant="outlined" size="small" onClick={() => navigate(`/product/cpucooler`)}>Back</Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+{/* 
             <div> Name <input placeholder={data[keys[4]]} aria-label="{keys[4]}" type="text" name={keys[4]} onChange={e => handleChange(e)} /></div>
             <div> Description <input placeholder={data[keys[5]]} aria-label="{keys[5]}" type="text" name={keys[5]} onChange={e => handleChange(e)} /></div>
             <div> Price <input placeholder={data[keys[6]]} aria-label="{keys[6]}" type="number" name={keys[6]} onChange={e => handleChange(e)} /></div>
@@ -52,7 +77,7 @@ export default function EditCpuCooler() {
             <div className="button-section">
                 <button type="submit">Save</button>
                 <button variant="outlined" size="small" onClick={() => navigate(`/product/cpucooler`)}> Back </button>
-            </div>
+            </div> */}
         </form>
     )
 }
