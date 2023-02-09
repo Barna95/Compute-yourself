21f15970822ac71293f7546897668f3b86ba8d3c
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-02-10 00:01:23 +0100

    update : RequireAuth
    bugfix

--- ComputeYourself/Compute Yourself/frontend/src/componentsFolder/RequireAuth.jsx
+++ ComputeYourself/Compute Yourself/frontend/src/componentsFolder/RequireAuth.jsx
@@ -1,14 +1,16 @@
 ﻿import { useLocation, Navigate, Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 
+
 const RequireAuth = ({ allowedRoles }) => {
     const { auth } = useAuth();
     const location = useLocation();
+    const loggedIn = localStorage.getItem("isLoggedIn");
 
     return (
         auth?.roles?.find(role => allowedRoles?.includes(role))
             ? <Outlet /> :
-            auth?.user ? <Navigate to="/error" state={{ from: location }} replace />
+            loggedIn ? <Navigate to="/unauthorized" state={{ from: location }} replace />
                 : <Navigate to="/account/login" state={{ from: location }} replace/>
         )
 }
