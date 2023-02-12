d9907762b1c0d79c85fab9b5ec592dd7326f147b
Barnabás Bálint <101584744+Barna95@users.noreply.github.com>
2023-02-12 14:33:49 +0100

    update : DeleteButtons
    using the FetchWithAxios file now for fetching, and also they trigger the current product parents fetch now to refetch the data the user deletes one

--- ComputeYourself/Compute Yourself/frontend/src/routes/DeleteProducts/DeleteButtonCard.jsx
+++ ComputeYourself/Compute Yourself/frontend/src/routes/DeleteProducts/DeleteButtonCard.jsx
@@ -1,31 +1,15 @@
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import React from 'react';
-import axios from "axios";
 import useAuth from "../../hooks/useAuth";
-import { toast } from 'react-toastify';
+import { AxiosDelete } from "../../Axios/FetchWithAxios"
 
 export default function DeleteButtonCard(props) {
-
     const { auth } = useAuth();
-    const  deleteFunction = async ()=>{
-        await axios.delete(`https://localhost:7195/product/${props.productType}/${props.productId}`, {
-            headers: {
-                'Content-Type': 'application/json;charset=UTF-8',
-                'Authorization': `Bearer ${auth.token}`
-            }
-        }).then(response => {
-            if (response.status === 200) {
-                toast.success('Successfully deleted.', {
-                    position: toast.POSITION.TOP_RIGHT
-                });
-            }
-        }).catch((error) => {
-            console.log(error.config);
-            toast.error('I am a toaster, cannot delete', {
-                position: toast.POSITION.TOP_RIGHT
-            });
-        })
+    const setTrigger = props.trigger;
+    const deleteFunction = async () => {
+        AxiosDelete(props.productType, props.productId, auth.token);
+        setTrigger("true");
         };
   return (
         <Button variant="outlined" size="small" onClick={deleteFunction} color='error' startIcon={<DeleteIcon />}>Delete</Button>
--- ComputeYourself/Compute Yourself/frontend/src/routes/DeleteProducts/DeleteButtonDetails.jsx
+++ ComputeYourself/Compute Yourself/frontend/src/routes/DeleteProducts/DeleteButtonDetails.jsx
@@ -1,36 +1,17 @@
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import React from 'react';
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
-import { toast } from 'react-toastify';
+import { AxiosDelete } from "../../Axios/FetchWithAxios"
 
 export default function DeleteButtonForDetails(props) {
     const navigate = useNavigate();
     const { auth } = useAuth();
-    const  deleteFunction = async ()=>{
-        await axios.delete(`https://localhost:7195/product/${props.productType}/${props.productId}`,
-            {
-            headers: {
-                'Content-Type': 'application/json;charset=UTF-8',
-                'Authorization': `Bearer ${auth.token}`
-            }
-            }).then(response => {
-                if (response.status === 200) {
-                    toast.success('Successfully deleted.', {
-                        position: toast.POSITION.TOP_RIGHT
-                    });
-                    return navigate(`/product/${props.productType}`)
-                }
-            }).catch((error) => {
-                console.log(error.config);
-                toast.error('I am a toaster, cannot delete', {
-                    position: toast.POSITION.TOP_RIGHT
-                });
-            })
+    const deleteFunction = async () => {
+        AxiosDelete(props.productType, props.productId, auth.token);
+        return navigate(`/product/${props.productType}`)
     };
-
 
   return (
         <Button variant="outlined" size="small" onClick={deleteFunction} color='error' startIcon={<DeleteIcon />}>Delete</Button>
