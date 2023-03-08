e991bfe52a878fbffabc7908bbeba3d980caa6aa
Erik <101416162+Eriknpy@users.noreply.github.com>
2023-03-08 12:24:14 +0100

    create : BasketController
    + GetBasket()
    ~ AddItemToBasket()
    ~ RemoveBasketItem()

---
+++ ComputeYourself/Compute Yourself/Controllers/BasketController.cs
@@ -0,0 +1,52 @@
+﻿using ComputeYourself.Data;
+using ComputeYourself.Migrations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComputeYourself.Controllers
+{
+    public class BasketController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public BasketController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Basket>> GetBasket()
+        {
+            var basket = await _context.Baskets
+                .Include(i => i.Items)
+                .ThenInclude(p => p.Product)
+                .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+
+            if (basket == null)
+            {
+                return NotFound();
+            }
+            return Ok(basket);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
+        {
+            // get basket
+            // create basket
+            // get product
+            // get item
+            // save changes
+            return StatusCode(201);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> RemoveBasketItem(int productId, int quantity)
+        {
+            // get basket
+            // remove item or reduce quantity
+            // save changes
+            return Ok();
+        }
+    }
+}
