ca60bb0bab168057a46442515499ae678806674e
Erik <101416162+Eriknpy@users.noreply.github.com>
2023-03-08 12:01:53 +0100

    create : Basket & BasketItems
    Basket
    + AddItem()
    + RemoveItem()
    + Items nav prop
    BasketItem
    + Specified table name
    + Navigation properties

---
+++ ComputeYourself/Compute Yourself/Models/Basket/Basket.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Identity.Client;
+
+namespace ComputeYourself.Models.Basket
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+        public string BuyerId { get; set; }
+
+        // Initialize a new collection every time. Therefore it cannot be null.
+        public List<BasketItem> Items { get; set; } = new();
+
+        public void AddItem(Product product, int quantity)
+        {
+            if (Items.All(item => item.ProductId != product.Id))
+            {
+                Items.Add(new BasketItem{Product = product, Quantity = quantity});
+            }
+
+            var existingItem = Items.FirstOrDefault(item => item.ProductId == product.Id);
+            if (existingItem != null)
+            {
+                existingItem.Quantity += quantity;
+            }
+        }
+
+        public void RemoveItem(int productId, int quantity)
+        {
+            var item = Items.FirstOrDefault(item => item.ProductId == productId);
+            if (item == null) return;
+            item.Quantity -= quantity;
+            if (item.Quantity == 0)
+            {
+                Items.Remove(item);
+            }
+        }
+    }
+}
---
+++ ComputeYourself/Compute Yourself/Models/Basket/BasketItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using MessagePack;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComputeYourself.Models.Basket;
+
+[Table("BasketItems")]
+public class BasketItem
+{
+    
+    public int Id { get; set; }
+    public int Quantity { get; set; }
+
+    // Navigation properties
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+
+    public int BasketId { get; set; }
+    public Basket Basket { get; set; }
+}
